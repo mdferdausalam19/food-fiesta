6ae3b3102918bc9c993219ad05823e7ac4ba512d
mdferdausalam19 <mdferdausalam333@gmail.com>
2024-09-21 22:14:21 +0600

    Implemented navbar and banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container mx-auto w-10/12 max-w-[1320px]">
+        {/* navbar-section */}
+        <Navbar></Navbar>
+        {/* banner-section */}
+        <Banner></Banner>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <>
+      <div className="hero min-h-[50vh] md:min-h-[80vh] bg-[url('./src/assets/images/banner.png')] bg-no-repeat bg-center bg-cover object-cover rounded-2xl mt-4">
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-4xl">
+            <h1 className="mb-6 text-5xl font-bold text-white">
+              Discover an exceptional cooking class tailored for you!
+            </h1>
+            <p className="mb-6 text-white text-lg">
+              Learn and Master Basic Programming, Data Structures, Algorithm,
+              OOP, Database and solve 500+ coding problems to become an
+              exceptionally well world-class Programmer.
+            </p>
+            <div className="space-x-4">
+              <button className="bg-[#0BE58A] p-4 rounded-full text-[#150B2B] font-semibold text-xl">
+                Explore Now
+              </button>
+              <button className="p-4 rounded-full border  text-white font-semibold text-xl">
+                Our Feedback
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
